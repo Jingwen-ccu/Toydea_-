07d1f24a5b7c2ce24bf41c05c320c0467382d657
Godzilla810 <e94106232@gs.ncku.edu.tw>
2024-01-19 15:35:24 +0900

    Add bonus2

---
+++ カービィ/2/2_bonus.rb
@@ -0,0 +1,20 @@
+require 'csv'
+
+# 讀取 CSV 檔案
+csv_file_path = 'r3-kenritsu-jidosho.the-best.csv'
+csv_data = CSV.read(csv_file_path, headers: true)
+csv_data.delete(0)
+
+# 設定 txt 檔案
+file_path = File.expand_path("2_bonus.csv")
+file = CSV.open(file_path, 'a')
+
+# ソート順：利用回数(多い方が前) > 複本数(少ない方が前) > 出版時間(古い方が前)
+sorted_data = csv_data.sort_by { |row| [-row[6].to_i, row[5].to_i, row[4], 
+# > 作者(英語 A-Z > 50 音順 > 漢字はソートしなくて、後ろに置く)
+                                        row[2].split.first] }
+file.to_io.write "\uFEFF"
+file.puts ["順位", "資料名", "著者名", "出版者", "出版年", "複本数", "利用回数"]
+sorted_data.each do |row|
+    file.puts row
+end 
