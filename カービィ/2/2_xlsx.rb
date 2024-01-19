9637430e4abec3d74ae4a0701aa5601fa5cc4aca
Godzilla810 <e94106232@gs.ncku.edu.tw>
2024-01-19 17:32:19 +0900

    Add bonus2.3_xlsx

---
+++ カービィ/2/2_xlsx.rb
@@ -0,0 +1,6 @@
+require_relative '../csv2xlsx'
+
+input_path = '2_csv.csv'
+output_path = '2_xlsx.xlsx'
+
+convert_csv_to_xlsx(input_path, output_path)
---
+++ カービィ/3/3_xlsx.rb
@@ -0,0 +1,6 @@
+require_relative '../csv2xlsx'
+
+input_path = '3_csv.csv'
+output_path = '3_xlsx.xlsx'
+
+convert_csv_to_xlsx(input_path, output_path)
